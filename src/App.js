829c9689b9d557d5b270e4f94a56c95410cd0ac1
DanielaS-Tochi <danielastochi@gmail.com>
2024-12-21 15:59:44 -0300

    Header and Link

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 //Para que no sea un nombre tan largo, podemos renombrarlo con ... as ...)
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Page404 from './pages/Page404';
+import Header from './components/Header';
 
 function App() {
 
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sobre' element={<Sobre />} />
-
+        <Route path='*' element={<Page404 />} />
       </Routes>
     </Router>
   );
